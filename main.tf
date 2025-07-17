b7a02fa572d77c5961ba388776d5db9f1221784a
varoonsahgal <varoon.sahgal@gmail.com>
2025-07-17 15:06:17 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,16 @@ provider "azurerm" {
   subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc"
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "vs-state-rg-pipeline"
+    storage_account_name = "vsstatesapipeline"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
 resource "azurerm_resource_group" "rg" {
 name     = "vs-basic-pipeline" #change the name please!
 location = "westus2"
